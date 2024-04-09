30f057b753bf2568f0f0ed8b90b7573f280df617
naufalbasara <naufalbasara@outlook.com>
2024-04-10 01:17:35 +0700

    chore: minor changes on cron update logic

--- src/scheduler.py
+++ src/scheduler.py
@@ -1,8 +1,9 @@
-from utils.utils import get_pardir, pardir
-import logging, json, os, sys
+from utils.utils import pardir, get_user
+import logging, json, os, sys, re
 logging.basicConfig(filename=f'{pardir}/log/running.log', filemode='a', level=logging.INFO, format=' %(asctime)s -  %(levelname)s:  %(message)s')
 
 from utils.scraper import fetch_prayertime, get_location
+from crontab import CronTab
 
 if __name__ == '__main__':
     """
@@ -22,8 +23,23 @@ if __name__ == '__main__':
     prayertime_data = fetch_prayertime(location=location, type='daily')
     fp = os.path.join(pardir, 'data.json')
     
+    # Load data to json file
     with open(fp, 'w') as to_write:
         json.dump(prayertime_data, to_write)
         logging.info('Prayer time data updated to data.json')
 
-    
+    # Update CRON Jobs
+    if len(sys.argv) == 1:
+        current_user = get_user()
+        cron = CronTab(user=f'{current_user}')
+
+        for prayer, time in prayertime_data['Daily Schedule'].items():
+            job = next(cron.find_comment(re.compile(f'^prife-{prayer}')))
+            hour, minute = time.split(":")
+            end = re.search(f'{prayer}-', job.command).end()
+            job.command = job.command[:end] + time
+            job.setall(f'{minute} {hour} * * *')
+            job.enable()
+        
+        cron.write_to_user(user=current_user)
+        logging.info('Prayer time data updated to cron job')
